9c5ee528c850dc8a6ccb38ce41b41d29947806e8
AmauryD <deflorenne.amaury@gmail.com>
2023-11-22 16:42:57 +0100

    fix types for genericDiff

--- test-app/tests/integration/utils/generic-diff-test.ts
+++ test-app/tests/integration/utils/generic-diff-test.ts
@@ -1,18 +1,24 @@
 import { module, test } from 'qunit';
 import genericDiff from '@triptyk/ember-utils/utils/generic-diff';
+import { setupTest } from 'ember-qunit';
 
-module('Integration | Util | generic-diff', function () {
+module('Integration | Util | generic-diff', function (hooks) {
+  setupTest(hooks);
+
   test('should return deleted and created object', async function (assert) {
+    const store = this.owner.lookup('service:store');
+
     const modelsData = [
-      {
+      store.createRecord('company', {
         id: '1',
         name: 'deleted',
-      },
-      {
+      }),
+      store.createRecord('company', {
         id: '2',
         name: 'persist',
-      },
+      }),
     ];
+
     const arrayData = [
       {
         id: '2',
@@ -24,20 +30,14 @@ module('Integration | Util | generic-diff', function () {
       },
     ];
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const { created, deleted } = genericDiff(modelsData as any, arrayData);
-    assert.strictEqual(
-      JSON.stringify(created),
-      JSON.stringify([{ name: 'created' }]),
-    );
-    assert.strictEqual(
-      JSON.stringify(deleted),
-      JSON.stringify([
-        {
-          id: '1',
-          name: 'deleted',
-        },
-      ]),
-    );
+    const { left, deleted } = genericDiff(modelsData, arrayData);
+
+    assert.deepEqual(left, [
+      {
+        id: undefined,
+        name: 'created',
+      },
+    ]);
+    assert.deepEqual(deleted, [modelsData[0]]);
   });
 });
